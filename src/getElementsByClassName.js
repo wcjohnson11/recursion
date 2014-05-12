a1e3fa076f7ef7a0125d5f8eeb4bd99f2910086a
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-05-11 22:23:01 -0700

    get elements by tag name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className, currentNode){
   // your code here
+	var results = [];
+	if (!currentNode){
+		currentNode = document.body;
+	}
+	var checkNode = function(node){
+	if(node.classList){
+		if (node.classList.contains(className)){
+			results.push(node);
+		}
+		if (node.hasChildNodes){
+			_.each(node.childNodes, checkNode);
+		}
+	}
+	};
+  checkNode(currentNode);
+  return results;
 };
