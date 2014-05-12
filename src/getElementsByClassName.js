ca4eafd5900f7b3a680a4ba81db8eb704e5f2a6a
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-05-12 11:31:12 -0700

    Hooray stringify

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,15 +6,18 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, currentNode){
   // your code here
+  //If no currentNode specified,currentNode = document.body;
 	var results = [];
 	if (!currentNode){
 		currentNode = document.body;
 	}
+	//check the nodes' classlist and if it contains className, push that node to results
 	var checkNode = function(node){
 	if(node.classList){
 		if (node.classList.contains(className)){
 			results.push(node);
 		}
+		//If it has child nodes, call checkNode on each child
 		if (node.hasChildNodes){
 			_.each(node.childNodes, checkNode);
 		}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,58 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+//Type checking null/number/boolean/string
+ if (obj === null) {
+	return 'null';
+  }
+
+ if (typeof obj === "number") {
+	return obj.toString();
+  }
+
+
+ if (typeof obj === "boolean") {
+	return (obj) ? 'true' : 'false';
+  }
+
+ if (typeof obj === "string") {
+	return '"' + obj + '"';
+  }
+
+  //Typechecking if array and concatenating array into JSON array
+  if (Array.isArray(obj)) {
+		var arrResult = '[';
+		_.each(obj, function(value, index){
+			arrResult += stringifyJSON(obj[index]) + ',';
+		});
+
+		//sayonara trailing commas
+	if (arrResult[arrResult.length-1] === ",") {
+		arrResult = arrResult.slice(0,-1);
+	}
+
+		arrResult += ']';
+		return arrResult;
+  }
+
+  //Typechecking if object (!== Array && null) and concatenating into JSON object
+  if (typeof obj === "object" && !Array.isArray(obj) && obj !== null) {
+	var objResult = '{';
+
+	_.each(obj, function(value, key, list){
+		//ignore functions and undefineds
+		if (typeof obj[key] !=="function" && typeof obj[key] !== "undefined"){
+			objResult += '"' + key + '":' + stringifyJSON(obj[key]) + ",";
+		}
+	});
+
+	//sayonara trailing commas
+	if (objResult[objResult.length-1] === ",") {
+		objResult = objResult.slice(0,-1);
+	}
+
+	objResult += '}';
+	return objResult;
+  }
+  
 };
